7c827414707e20980a065768d9aed5e1bf892a6d
Ivan Varyuhin <varyuhin03@mail.ru>
2025-03-09 20:51:20 +0300

    fix: requestMapping

--- google-docs/src/main/java/com/example/googledocs/controller/ApiManagerController.java
+++ google-docs/src/main/java/com/example/googledocs/controller/ApiManagerController.java
@@ -4,10 +4,12 @@ import com.example.googledocs.core.ApiManagerService;
 import com.example.googledocs.domain.entity.Module;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
-@RestController("api/v1")
+@RestController
+@RequestMapping("api/v1")
 public class ApiManagerController {
 
     private final ApiManagerService apiManagerService;
